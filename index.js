ca67f27082ec5cc3c9e2b97e2b1d1c4e7acf239a
Eric Jiang <ericjiang97@gmail.com>
2018-11-01 16:15:14 +1100

    added port

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
  
+const port = process.env.PORT || 8080
+
 // Create service provider
 var sp_options = {
   entity_id: "./metadata.xml",
@@ -79,4 +81,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000);
+app.listen(port);
